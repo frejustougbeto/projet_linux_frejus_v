915a5edecaba97065de8603df264e845154c8a9b
Frejus TOUGBETO <frejustougbeto@yahoo.fr>
2018-12-01 20:39:31 +0100

    Ajout du script sur la nouvelle branche: branch_projet

--- fich_semaine_derniere.sh
+++ fich_semaine_derniere.sh
@@ -4,5 +4,5 @@ echo "Hello $USERNAME, nous sommes le $(date +"%d %B %Y")."
 read -p  "Veuillez entrer le chemin du repertoire souhaité: " repertoire ###recueillir le chemin du repertoire
 echo "  "   ### Saut de lignes pour plus de clarté
 
-ls $repertoire ### Affichage du repertoire fourni
+find $repertoire -type f -mtime -7  ### Affichage de tous les fichiers modifiés il y a moins d'une semaine (7 jours)
  
